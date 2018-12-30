9c05f01573479d6f55a662c49783d1993689933a
sushant12 <sussyoung9@gmail.com>
2018-12-30 12:56:39 +0545

    added missing columns for fb page table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_29_065313) do
+ActiveRecord::Schema.define(version: 2018_12_30_070956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_12_29_065313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id"
+    t.string "name"
+    t.string "fb_page_id"
     t.index ["user_id"], name: "index_fb_pages_on_user_id"
   end
 
