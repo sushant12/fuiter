f17fc3d461cbe01854b3a8e2a96637b05a983020
sushant12 <sussyoung9@gmail.com>
2018-12-30 16:17:43 +0545

    setup wrapper for koala

---
+++ lib/facebook/page.rb
@@ -0,0 +1,19 @@
+module Facebook
+  class Page
+    def initialize(token)
+      @token = token
+    end
+
+    def list
+    end
+
+    def get_info
+    end
+
+    def sync
+    end
+
+    private
+    
+  end
+end
