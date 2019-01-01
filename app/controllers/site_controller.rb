f23160ee436ffbce2487ff6703ace3ef347ec751
sushant12 <sussyoung9@gmail.com>
2019-01-01 17:24:17 +0545

    show slider image

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,19 @@ class SiteController < ApplicationController
   layout "templates/ucorpora"
 
   def home
+    @page = FbPage.find_by(id: "91087338-4e93-41a7-a76d-c59cf2709479")
+    @sliders = @page.content['albums']['data'].select do |slider|
+      slider['name'] == "Cover Photos"
+    end
+    # binding.pry
+    @abc = @sliders.first["photos"]["data"].select do |a|
+      a["images"].first
+    end
+    # binding.pry
+
+    # binding.pry
+    # render json: @page.content
+
     render template: "templates/ucorpora/home"
   end
 
--- app/javascript/packs/templates/ucorpora/ucorpora.js
+++ app/javascript/packs/templates/ucorpora/ucorpora.js
@@ -3,4 +3,3 @@ import '../../../templates/ucorpora/styles/flexslider'
 import '../../../templates/ucorpora/js/functions'
 import '../../../templates/ucorpora/js/jquery.flexslider'
 require.context('../../../templates/ucorpora/images', true)
-/*import '../../../templates/ucorpora/images/logo-header.png'*/
