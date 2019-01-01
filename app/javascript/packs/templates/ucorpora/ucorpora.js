0af77d6c10b0368a05ff9824cdef31f699212a44
sushant12 <sussyoung9@gmail.com>
2019-01-01 15:27:14 +0545

    better way to import all the images at once

--- app/javascript/packs/templates/ucorpora/ucorpora.js
+++ app/javascript/packs/templates/ucorpora/ucorpora.js
@@ -2,4 +2,5 @@ import '../../../templates/ucorpora/styles/style'
 import '../../../templates/ucorpora/styles/flexslider'
 import '../../../templates/ucorpora/js/functions'
 import '../../../templates/ucorpora/js/jquery.flexslider'
-import '../../../templates/ucorpora/images/logo-header.png'
+require.context('../../../templates/ucorpora/images', true)
+/*import '../../../templates/ucorpora/images/logo-header.png'*/
