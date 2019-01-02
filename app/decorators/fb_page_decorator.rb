e2d7cdfed14f0563dcf836528abf60f77484880a
sushant12 <sussyoung9@gmail.com>
2019-01-02 07:45:23 +0545

    created methods to print in templates

--- app/decorators/fb_page_decorator.rb
+++ app/decorators/fb_page_decorator.rb
@@ -10,4 +10,62 @@ class FbPageDecorator < Draper::Decorator
   #     end
   #   end
 
+  def cover_images
+    albums = object.content['albums']['data'].select do |album|
+      album['name'] == "Cover Photos"
+    end
+    albums.first["photos"]["data"].map do |img|
+      img["images"].first
+    end unless albums.empty?
+  end
+
+  def name
+    object.content['name']
+  end
+
+  def address
+    object.content['location']
+  end
+
+  def albums
+    object.content['albums']['data'].map do |album|
+      {
+        album["name"] => photos(album['id']).first.first['source'] 
+      }
+    end
+  end
+
+  def about
+    object.content['about']
+  end
+
+  def posts
+    object.content['feed']['data'].map do |feed|
+      {
+        "message" => feed['message'],
+        "image" => feed['full_picture']
+      }
+    end
+  end
+
+  def events
+    object.content['events']['data'].map do |event|
+      {
+        "name" => event['name'],
+        "location" => event['place'],
+        "details" => event['description']
+      }
+    end
+  end
+
+  private
+
+  def photos(album_id)
+    albums = object.content['albums']['data'].select do |album|
+      album["id"] == album_id
+    end
+    albums.first['photos']['data'].map do |photo|
+      photo["images"]
+    end
+  end
 end
--- lib/facebook/page.rb
+++ lib/facebook/page.rb
@@ -9,7 +9,7 @@ module Facebook
     end
 
     def get_info
-      @graph.get_connections("me", "?fields=about,bio,company_overview,contact_address,cover,current_location,description,description_html,founded,general_info,access_token,location,mission,name,albums{description,name,count,photo_count,photos{images}},feed{description,full_picture,message,picture,attachments{description,media,url},type},events{cover,description,end_time,event_times,name,place,start_time,ticket_uri}")
+      @graph.get_connections("me", "?fields=about,bio,company_overview,contact_address,current_location,description,description_html,founded,general_info,access_token,location,mission,name,albums{description,name,count,photo_count,photos{images}},feed{description,full_picture,message,picture,attachments{description,media,url},type},events{cover,description,end_time,event_times,name,place,start_time,ticket_uri}")
     end
   end
 end
