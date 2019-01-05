aa2173d315326e8c8b5bec5b3812a82473c0cf43
sushant12 <sussyoung9@gmail.com>
2019-01-05 18:57:22 +0545

    fixing load path

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,9 @@ module Fuitter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.autoload_paths += %W[#{config.root}/lib]
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.eager_load_paths << "#{Rails.root}/lib"
+    load_path_strategy = Rails.env.production? ? :eager_load_paths : :autoload_paths
+    config.public_send(load_path_strategy) << Rails.root.join('lib')
+
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.generators do |g|
