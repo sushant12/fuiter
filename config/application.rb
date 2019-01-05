873e3f101824d99a16bf0185370a39ea5c0920bd
sushant12 <sussyoung9@gmail.com>
2019-01-05 18:14:47 +0545

    fixed autoload issue

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,6 @@ module Fuitter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
