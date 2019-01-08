4facf907fd31b033c078845b7e39b72b6fda1e45
sushant12 <sussyoung9@gmail.com>
2019-01-08 19:10:57 +0545

    spec for fb_page_decorator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,11 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 
+require 'factory_bot'
+require 'database_cleaner'
+
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -46,7 +50,17 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
