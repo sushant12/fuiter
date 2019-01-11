131adfb2895dbf894d1b5661e5cabbd822e554c4
SaraswatiSaud <saraswati.saud@gmail.com>
2019-01-11 22:58:01 +0545

    Create contact page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,22 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'grasruts.com' }
+  # config.action_mailer.smtp_settings = {
+  #   address:              ENV['SMTP_ADDRESS'],
+  #   port:                 ENV['SMTP_PORT'],
+  #   domain:               ENV['SMTP_DOMAIN'],
+  #   user_name:            ENV['SMTP_USER_NAME'],
+  #   password:             ENV['SMTP_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = { from: 'contact@grasruts.com' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
