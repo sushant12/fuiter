635cd76561876cc5f5050306e3f10e06541c034e
shreejana012 <shrijana460@gmail.com>
2019-01-15 10:30:06 +0545

    fb page template properties

--- app/javascript/views/editor.vue
+++ app/javascript/views/editor.vue
@@ -1,17 +1,34 @@
 <template>
-  <div id="sidebar">
-    <chrome-picker v-model="color" @input="changeColor"></chrome-picker>
-    font family: <input type="text" v-model="font" @focusout='changeFont'> <br>
-    site title: <input type="text" v-model="font" @focusout='changeTitle'> <br>
-  </div>
+  <div class="sidenav">
+    <div class = "sideContent">
+      <p class="p-label">Pick Color</p>
+        <chrome-picker v-model="color" @input="changeColor">
+        </chrome-picker>
+      <hr />
+      <p class="p-label">Site Title</p><input type="text" v-model="title" @focusout='changeTitle'>
+      <hr />
+      <p class="p-label">Font Family</p>
+      <select v-model="selectedFont" @change='changeFont'>
+        <option :value="null">Choose Font</option>
+        <option 
+          v-for="font in fonts" 
+          :key="font" 
+          :style="{fontFamily: font }"
+          >
+          {{font}}
+        </option>
+      </select>
+      </div>
+   </div>
 </template>
 
 <script>
 import _ from 'lodash';
+import EditorServices from '../services/index.js';
 import { Chrome } from 'vue-color';
 
 export default {
-  props: ['template'],
+  props: ['template','fbPageId'],
   data() {
     return {
       color: {
@@ -21,6 +38,16 @@ export default {
       logo: '',
       title: '',
       tmpl: {},
+      selectedFont: null,
+      fonts: [
+          'Arial',
+          'Helvetica',
+          'Times New Roman',
+          'Times',
+          'Courier New',
+          'Courier',
+          'Verdana'
+       ]
     };
   },
   methods: {
@@ -31,25 +58,31 @@ export default {
       _.each(colorClass, (el) => {
         el.style.color = color;
       });
+      this.updateProperty();
     },
     changeFont(e){
       const font = e.target.value;
       const frame = document.getElementById('frame').contentDocument.body;
       frame.style.fontFamily = font;
+      this.updateProperty();
     },
     changeTitle(e) {
       const title = e.target.value;
       const frame = document.getElementById('frame').contentDocument.head;
       frame.title = title;
+      this.updateProperty();
     },
     updateProperty() {
       this.tmpl = {
-        font: this.font,
-        color: this.color,
-        logo: this.logo,
-        title: this.title
+        font: this.selectedFont || '',
+        color: this.color['hex'] || '',
+        logo: this.logo || '',
+        title: this.title || '',
+        template_id: this.template.id,
+        fb_page_id: this.fbPageId
       };
-    },
+    EditorServices.updateProperties(this.tmpl)
+    }
 
   },
   mounted() {
@@ -74,5 +107,23 @@ export default {
 </script>
 
 <style scoped>
-
+  .sidenav {
+    height: 100%;
+    width: 250px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #4c5764;
+  }
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+  }
+  .p-label {
+    color: white;
+  }
+  .sideContent{
+    margin: 10px;
+  }
 </style>
