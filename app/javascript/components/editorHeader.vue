479c97b468a114b9b59aeb238267e7e9d83599b8
SaraswatiSaud <saraswati.saud@gmail.com>
2019-01-19 00:18:15 +0545

    Add changes to dashboard

--- app/javascript/components/editorHeader.vue
+++ app/javascript/components/editorHeader.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="main-navbar">
-    <div class="device-selections">
-      <span data-device="monitor" class="demo-device-icon-monitor is-active">
-        <a class="icon-link" name="monitor" value="monitor" @click="updateResolution('monitor')" href="#">
-          <i class="fas fa-desktop"></i>
-        </a>
-      </span>
-      <span data-device="tablet" class="demo-device-icon-tablet">
-        <a class="icon-link" name="tablet" value="tablet" @click="updateResolution('tablet')">
-          <i class="fas fa-tablet-alt"></i>
-        </a>
-      </span>
-      <span data-device="phone" class="demo-device-icon-phone">
-        <a class="icon-link" name="phone" value="phone" @click="updateResolution('phone')">
-          <i class="fas fa-mobile-alt"></i>
-          </a>
-      </span>
-    </div>
+  <div class="device-selections">
+    <span data-device="monitor">
+      <a name="monitor" value="monitor" @click="updateResolution('monitor')">
+        <i class="fa fa-desktop fa-2x"></i>
+      </a>
+    </span>
+    <span data-device="tablet">
+      <a name="tablet" value="tablet" @click="updateResolution('tablet')">
+        <i class="fa fa-tablet fa-2x"></i>
+      </a>
+    </span>
+    <span data-device="phone">
+      <a name="phone" value="phone" @click="updateResolution('phone')">
+        <i class="fa fa-tablet fa-2x"></i>
+      </a>
+    </span>
   </div>
 </template>
 <script>
