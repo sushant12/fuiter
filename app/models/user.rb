49a81ae0311f4ec508ec6d0b49df649ab5f00d69
SaraswatiSaud <saraswati.saud@gmail.com>
2019-02-03 17:08:35 +0545

    Add enum to user category

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
 
+  enum category: { normal: 'normal', reseller: 'reseller', admin: 'admin' }
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
