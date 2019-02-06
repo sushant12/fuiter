9c74a76fcdf6286989deef270b257af77c294372
sushant12 <sussyoung9@gmail.com>
2019-02-06 18:22:06 +0545

    fixed rspec

--- spec/factories/fb_page_template.rb
+++ spec/factories/fb_page_template.rb
@@ -2,5 +2,32 @@
 
 FactoryBot.define do
   factory :fb_page_template do
+    pages do
+      [
+        {"uri": "about", 
+          "title": "About", 
+          "nested": [{
+              "uri": "events", 
+              "title": "Events", 
+              "position": 3
+            }], 
+          "position": 2}, 
+        {"uri": "contact", 
+         "title": "Contact",
+         "nested": [],
+         "position": 5},
+        {"uri": "home",
+         "title": "Home",
+         "nested": [],
+         "position": 1},
+        {"uri": "news",
+         "title": "News",
+         "nested": [],
+         "position": 6},
+        {"uri": "gallery",
+         "title": "Gallery",
+         "nested": [],
+         "position": 4}]
+    end
   end
 end
--- spec/factories/page.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :page do
-    title { 'Home' }
-    uri { 'home' }
-  end
-end
