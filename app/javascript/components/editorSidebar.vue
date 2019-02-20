d29849dd21e0ddffeb6b8d5cbfbf621beb9d49b3
jyapujuju <danieldangol@gmail.com>
2019-02-20 22:06:32 +0545

    fix design issue for editor

--- app/javascript/components/editorSidebar.vue
+++ app/javascript/components/editorSidebar.vue
@@ -1,42 +1,51 @@
 <template>
-<nav class="child-menu is-dark is-sidebar-translated">
-  <div v-if="showParentMenu" class="child-menu-inner">
-    <ul class="sidebar-menu is-active animated" id="dashboard-menu">
-      <li class="have-children">
-        <a class="parent-link active" @click="showMenu('Design')">Design</a>
-      </li>
-      <li class="have-children">
-        <a class="parent-link " @click="showMenu('Page')">Pages</a>
-      </li>
-      <li class="have-children">
-        <a class="parent-link " @click="showMenu('Setting')">Settings</a>
-      </li>
-    </ul>
-  </div>
-  <div v-else>
-    <keep-alive>
-      <component 
-      v-bind:is="currentMenu"
-      @clicked-main-menu='resetMenu'
-      :template='template'
-      :pages='pages'
-      :fb_page_id='fb_page_id'>
-      </component>
-    </keep-alive>
-  </div>
-</nav>
+  <nav class="child-menu is-sidebar-translated">
+    <div v-if="showParentMenu" class="child-menu-inner">
+      <ul class="sidebar-menu is-active animated" id="dashboard-menu">
+        <li class="have-children">
+          <a class="parent-link active" @click="showMenu('Design')">
+            <i class="fa fa-paint-brush"/>
+            Design
+          </a>
+        </li>
+        <li class="have-children">
+          <a class="parent-link" @click="showMenu('Page')">
+            <i class="fa fa-file"/>
+            Pages
+          </a>
+        </li>
+        <li class="have-children">
+          <a class="parent-link" @click="showMenu('Setting')">
+            <i class="fa fa-cogs"/>
+            Settings
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <keep-alive>
+        <component
+          v-bind:is="currentMenu"
+          @clicked-main-menu="resetMenu"
+          :template="template"
+          :pages="pages"
+          :fb_page_id="fb_page_id"
+        ></component>
+      </keep-alive>
+    </div>
+  </nav>
 </template>
 <script>
-import Design from './editorMenu/design.vue';
-import Page from './editorMenu/page.vue';
-import Setting from './editorMenu/setting.vue';
+import Design from "./editorMenu/design.vue";
+import Page from "./editorMenu/page.vue";
+import Setting from "./editorMenu/setting.vue";
 
 export default {
-  props: ['template', 'pages', 'fb_page_id'],
+  props: ["template", "pages", "fb_page_id"],
   data() {
     return {
       showParentMenu: true,
-      currentMenu: '',
+      currentMenu: ""
     };
   },
   methods: {
@@ -46,22 +55,25 @@ export default {
     },
     resetMenu() {
       this.showParentMenu = true;
-      this.currentMenu = '';
-    },
+      this.currentMenu = "";
+    }
   },
   components: {
     Design,
     Page,
-    Setting,
-  },
+    Setting
+  }
 };
 </script>
 <style scoped>
-  .child-menu-inner {
-    height: 550px;
-    overflow-x: hidden;
-    overflow-y: auto;
-    padding-right: 5px;
-  }
+.child-menu-inner {
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-right: 5px;
+  position: fixed;
+  width: 250px;
+  background-color: #373751;
+}
 </style>
 
