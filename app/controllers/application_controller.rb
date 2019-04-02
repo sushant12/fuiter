5360f5c55b878ebd804a2c6e462883a35a9bfd9f
SaraswatiSaud <saraswati.saud@gmail.com>
2019-04-02 19:19:13 +0545

    Redirect to login path after signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     dashboard_path
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   private
 
   def set_raven_context
