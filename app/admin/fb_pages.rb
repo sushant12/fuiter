eb10c2ce54927ee2e4c2ac174f1ca37ed5942a6a
SaraswatiSaud <saraswati.saud@gmail.com>
2019-04-09 20:23:24 +0545

    f-134 Make fb Pages editable

--- app/admin/fb_pages.rb
+++ app/admin/fb_pages.rb
@@ -1,8 +1,25 @@
 ActiveAdmin.register FbPage do
+  config.clear_action_items!
+
   index do
     column :name
     column :status
     column :category
     column :fb_page_id
+    actions
+  end
+
+  permit_params :name, :status, :category, :fb_page_id
+
+  form title: 'Facebook page form' do |f|
+    inputs 'Details' do
+      input :name
+      input :status, as: :select, collection: FbPage.statuses
+      input :category
+      input :fb_page_id
+    end
+
+    para "Press cancel to return to the list without saving."
+    actions
   end
 end
