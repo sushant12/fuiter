f52089a693396b03c5fa38d94fa862aae8a84888
sushant12 <sussyoung9@gmail.com>
2019-04-24 09:45:49 +0545

    integrated Riverina design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
